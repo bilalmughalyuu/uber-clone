72d299ad220641dc8320c70147d409cb86f16232
bilalmughalyuu <bilalmuhammad306@yahoo.com>
2025-07-02 15:36:25 +0400

    Add RideRequestView and UberX icon assets

---
+++ uberclone/Core/Trips/Views/RideRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  RideRequestView.swift
+//  uberclone
+//
+//  Created by Muhammad Bilal on 02/07/2025.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
